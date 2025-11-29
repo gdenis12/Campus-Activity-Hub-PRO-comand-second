d618d2ff31a66ffdf9fb3cde8f38540d0c00cd12
gdenis <gdenis@GDENIS>
2025-11-29 16:07:15 +0200

    Добавьте файлы проекта.

---
+++ Campus Activity Hub PRO comand second/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Campus_Activity_Hub_PRO_comand_second.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
